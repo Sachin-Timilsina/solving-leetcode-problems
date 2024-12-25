cbddf480969ba6f90a87894793c15f8bb72d3013
Sachin Timilsina <sachintimilsina11@gmail.com>
2024-12-25 18:23:52 +0545

    Update file 12/25/2024, 6:23:51 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,7 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for first_index, first_number in enumerate(nums):
-            for second_index in range(len(nums)):
-                if first_index is not second_index:
-                    if (first_number + nums[second_index]) == target:
-                        return [first_index, second_index]
+        visited = dict()
+        for index, number in enumerate(nums):
+            compliment = target - number
+            if compliment in visited:
+                return [index, visited[compliment]]
+            visited[number] = index
