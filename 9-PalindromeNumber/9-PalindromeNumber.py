f409f4d3e41a8a501b3ed24f3c48d068693fb779
Sachin Timilsina <sachintimilsina11@gmail.com>
2024-12-26 21:03:38 +0545

    Update file 12/26/2024, 9:03:38 PM

--- 9-PalindromeNumber/9-PalindromeNumber.py
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -1,13 +1,4 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0 or (x % 10 == 0 and x != 0):
-            return False
-        
-        reverse = 0
-        copy = x
-
-        while (copy != 0):
-            reverse = reverse * 10 + copy % 10
-            copy //= 10
-        
-        return reverse == x
+        xx = str(x)
+        return xx == xx[::-1]
