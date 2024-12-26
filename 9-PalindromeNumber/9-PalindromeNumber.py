1554038be82a13a422e87807b90988a5b4bfc96d
Sachin Timilsina <sachintimilsina11@gmail.com>
2024-12-26 21:02:30 +0545

    Update file 12/26/2024, 9:02:30 PM

--- 9-PalindromeNumber/9-PalindromeNumber.py
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -1,11 +1,4 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-            return False
-        duplicate_x = x
-        reverse_number = 0
-        while duplicate_x != 0:
-            last_digit = duplicate_x % 10
-            reverse_number = reverse_number * 10 + last_digit
-            duplicate_x = duplicate_x // 10
-        return reverse_number == x
+        xx = str(x)
+        return xx == xx[::-1]
